937151872761137ce4aae0bdb1c63603106a8d74
Shivam Srivastava <shivam01990@gmail.com>
2018-12-02 00:14:57 +0530

    modify the directory

---
+++ CodeAssignment.Web/Controllers/UsersController.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Mvc;
+using CodeAssgnment.Shared;
+using CodeAssignment.Web.Models;
+
+namespace CodeAssignment.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        private ServiceRepository _serviceRepository;
+
+        public UsersController()
+        {
+            _serviceRepository = new ServiceRepository();
+        }
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            HttpResponseMessage response = _serviceRepository.GetResponse("api/user");
+            response.EnsureSuccessStatusCode();
+            List<UserEntity> users = response.Content.ReadAsAsync<List<UserEntity>>().Result;
+            return View(users);
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            HttpResponseMessage response = _serviceRepository.GetResponse("api/user?id="+id.ToString());
+            response.EnsureSuccessStatusCode();
+            UserEntity user = response.Content.ReadAsAsync<UserEntity>().Result;
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,ForeNames,SurName,Dob,Gender,HomeNo,WorkNo,MobileNo,CreatedOn,UpdatedOn")] UserModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                HttpResponseMessage response = _serviceRepository.PostResponse("api/user", user.ConvertToEntity(user));
+                response.EnsureSuccessStatusCode();
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+
+            HttpResponseMessage response = _serviceRepository.GetResponse("api/user?id=" + id.ToString());
+            response.EnsureSuccessStatusCode();
+            UserEntity user = response.Content.ReadAsAsync<UserEntity>().Result;
+
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(new UserModel(user));
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,ForeNames,SurName,Dob,Gender,HomeNo,WorkNo,MobileNo,CreatedOn,UpdatedOn")] UserModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                HttpResponseMessage response = _serviceRepository.PutResponse("api/user", user.ConvertToEntity(user));
+                response.EnsureSuccessStatusCode();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            HttpResponseMessage response = _serviceRepository.GetResponse("api/user?id=" + id.ToString());
+            response.EnsureSuccessStatusCode();
+            UserEntity user = response.Content.ReadAsAsync<UserEntity>().Result;
+
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            HttpResponseMessage response = _serviceRepository.GetResponse("api/user?id=" + id.ToString());
+            response.EnsureSuccessStatusCode();
+            UserEntity user = response.Content.ReadAsAsync<UserEntity>().Result;
+            //db.Users.Remove(user);
+            //db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CodeAssignment.Web/Models/UserModel.cs
@@ -0,0 +1,69 @@
+﻿using CodeAssgnment.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodeAssignment.Web.Models
+{
+    public class UserModel
+    {
+        public UserModel()
+        { }
+
+        public UserModel(UserEntity model)
+        {
+            this.ForeNames = model.ForeNames;
+            this.SurName = model.SurName;
+            this.Gender = model.Gender;
+            this.CreatedOn = model.CreatedOn;
+            this.UpdatedOn = model.UpdatedOn;
+            this.MobileNo = model.MobileNo;
+            this.Id = model.Id;
+            this.Dob = model.Dob;
+            this.HomeNo = model.HomeNo;
+            this.WorkNo = model.WorkNo;
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [Range(3, 50, ErrorMessage = "Enter number between 3 to 50")]
+        public string ForeNames { get; set; }
+
+        [Required]
+        [Range(2, 50, ErrorMessage = "Enter number between 2 to 50")]
+        public string SurName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]        
+        public DateTime? Dob { get; set; }
+
+        [Required]
+        public bool Gender { get; set; }
+
+        public string HomeNo { get; set; }
+        public string WorkNo { get; set; }
+        public string MobileNo { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+
+        public UserEntity ConvertToEntity(UserModel model)
+        {
+            UserEntity obj = new UserEntity();
+            obj.ForeNames = model.ForeNames;
+            obj.SurName = model.SurName;
+            obj.Gender = model.Gender;
+            obj.CreatedOn = model.CreatedOn;
+            obj.UpdatedOn = model.UpdatedOn;
+            obj.MobileNo = model.MobileNo;
+            obj.Id = model.Id;
+            obj.Dob = model.Dob;
+            obj.HomeNo = model.HomeNo;
+            obj.WorkNo = model.WorkNo;
+            return obj;
+        }
+
+    }
+}
